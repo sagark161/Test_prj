040c343124eead979b8b1eaebd27eebce1b347ec
Sagar <sagar@Sagars-MacBook-Air.local>
2024-07-17 23:19:17 +0530

    commiting from branch

--- test/TestSuite1.test.ts
+++ test/TestSuite1.test.ts
@@ -3,8 +3,11 @@ describe('Test Suite1', () => {
         expect(1+3).toBe(4)
     })
 
-    //dipali changes
+    it('Testing git branch for pushing code',() =>{
+        expect(1+3).toBe(4)
+    })
+
 })
 
-//to stage
+
 
